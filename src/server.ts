d3faeebfc1db03a0d8564cb1a0639139fb71e775
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2025-08-06 21:35:34 +0530

    Implemented base server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import logger from './middleware/logger.ts';
 import { vehicleLogParser } from './service/log-parser.ts';
+import client, {connectCassandra, createKeySpace, createLogTable, useKeySpace} from "./db/client.ts";
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     logger.info(`Server running on port ${PORT}`);
+    await connectCassandra(client);
+    await createKeySpace(client);
+    await useKeySpace(client);
+    await createLogTable(client);
+
     vehicleLogParser('data/vehicle_diagnostics_logs.txt', logger);
 })
 
