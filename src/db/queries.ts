2ac3e353c6c6579b7ece55d36f71fff132d2d140
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2025-08-06 21:33:42 +0530

    Defined table queries

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,1 +1,17 @@
-export const insertVehicleLogQuery = 'INSERT INTO logs (id, timestamp, vehicle_id, log_level, code, message) VALUES (uuid(), ?, ?, ?, ?, ?)';
+export const insertVehicleLogQuery = 'INSERT INTO vehicle_logs (log_id, log_timestamp, vehicle_id, log_level, code, message, created_at, updated_at) VALUES (uuid(), ?, ?, ?, ?, ?, toTimestamp(now()), toTimestamp(now()))';
+
+export const createKeySpaceQuery = `CREATE KEYSPACE IF NOT EXISTS vehicle_keyspace WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 1 }`;
+
+export const useKeySpaceQuery = `USE vehicle_keyspace`;
+
+export const createVehicleLogTableQuery = `CREATE TABLE IF NOT EXISTS vehicle_logs (
+    log_id uuid PRIMARY KEY,
+    log_timestamp text,
+    vehicle_id text,
+    log_level text,
+    code text,
+    message text,
+    created_at timestamp,
+    updated_at timestamp,
+);
+`;
