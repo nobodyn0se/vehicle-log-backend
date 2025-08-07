8e424079f6c98442ebbe4c42c1c917791d551f63
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2025-08-07 14:59:02 +0530

    Added invalidating test case for log parser

--- tests/service/log-parser.test.ts
+++ tests/service/log-parser.test.ts
@@ -1,12 +1,16 @@
 import fs from 'fs';
 import { vehicleLogParser } from '../../src/service/log-parser.ts';
 import logger from '../../src/middleware/logger.ts';
-import {afterEach} from "node:test";
 import {insertVehicleLog} from "../../src/db/client.ts";
+import {validateLogDataFormat} from "../../src/util/util.ts";
 
 jest.mock('../../src/middleware/logger.ts');
 jest.mock('../../src/db/client.ts', () => ({
     insertVehicleLog: jest.fn(),
+}));
+jest.mock('../../src/util/util.ts', () => ({
+    ...jest.requireActual('../../src/util/util.ts'),
+    validateLogDataFormat: jest.fn() as unknown as jest.Mock,
 }));
 
 describe('Log parser tests', () => {
@@ -30,15 +34,30 @@ describe('Log parser tests', () => {
 
     it('should parse the vehicle logs correctly', async () => {
         (insertVehicleLog as jest.Mock).mockResolvedValueOnce(undefined);
+        (validateLogDataFormat as unknown as jest.Mock).mockReturnValue(true);
+
         await vehicleLogParser(testPath, mockLogger);
 
-        expect(mockLogger.info).toHaveBeenCalledWith('Successfully created file stream for the parser');
-        expect(mockLogger.info).toHaveBeenCalledWith('Parsing the file stream now...');
-
-        expect(mockLogger.info).toHaveBeenCalledWith('Wed Jan 01 2025 00:00:25 GMT+0530 (India Standard Time), 1013, WARN, U0420, Steering angle sensor malfunction');
-        expect(mockLogger.info).toHaveBeenCalledWith('Wed Jan 01 2025 00:00:30 GMT+0530 (India Standard Time), 1004, INFO, P0171, System too lean (Bank 1)');
-        expect(mockLogger.info).toHaveBeenCalledWith('Wed Jan 01 2025 00:00:35 GMT+0530 (India Standard Time), 1014, ERROR, U0420, Steering angle sensor malfunction');
+        expect(mockLogger.info).toHaveBeenCalledWith(
+            'Wed Jan 01 2025 00:00:25 GMT+0530 (India Standard Time), 1013, WARN, U0420, Steering angle sensor malfunction'
+        );
+        expect(mockLogger.info).toHaveBeenCalledWith(
+            'Wed Jan 01 2025 00:00:30 GMT+0530 (India Standard Time), 1004, INFO, P0171, System too lean (Bank 1)'
+        );
+        expect(mockLogger.info).toHaveBeenCalledWith(
+            'Wed Jan 01 2025 00:00:35 GMT+0530 (India Standard Time), 1014, ERROR, U0420, Steering angle sensor malfunction'
+        );
 
         expect(mockLogger.info).toHaveBeenCalledWith('3 records were parsed by the system');
+    });
+
+    it('should log invalid log format and not attempt insertion', async () => {
+
+        (validateLogDataFormat as unknown as jest.Mock).mockReturnValue(false);
+        await vehicleLogParser(testPath, mockLogger);
+
+        expect(mockLogger.error).toHaveBeenCalled();
+        expect(insertVehicleLog).not.toHaveBeenCalled();
+        expect(mockLogger.error).toHaveBeenCalledWith('Invalid log data cannot be inserted into DB');
     });
 });
