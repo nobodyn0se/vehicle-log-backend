75e1a0389b0a07f16c46a0ca5ef979a4bd0370cc
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2025-08-07 13:32:30 +0530

    Updated test case for log parser

--- tests/service/log-parser.test.ts
+++ tests/service/log-parser.test.ts
@@ -2,9 +2,12 @@ import fs from 'fs';
 import { vehicleLogParser } from '../../src/service/log-parser.ts';
 import logger from '../../src/middleware/logger.ts';
 import {afterEach} from "node:test";
-import readline from "readline";
+import {insertVehicleLog} from "../../src/db/client.ts";
 
 jest.mock('../../src/middleware/logger.ts');
+jest.mock('../../src/db/client.ts', () => ({
+    insertVehicleLog: jest.fn(),
+}));
 
 describe('Log parser tests', () => {
     const mockLogger = logger as jest.Mocked<typeof logger>;
@@ -26,14 +29,15 @@ describe('Log parser tests', () => {
 
 
     it('should parse the vehicle logs correctly', async () => {
+        (insertVehicleLog as jest.Mock).mockResolvedValueOnce(undefined);
         await vehicleLogParser(testPath, mockLogger);
 
         expect(mockLogger.info).toHaveBeenCalledWith('Successfully created file stream for the parser');
         expect(mockLogger.info).toHaveBeenCalledWith('Parsing the file stream now...');
 
-        expect(mockLogger.info).toHaveBeenCalledWith('2025-01-01 00:00:25, VEHICLE_ID:1013, WARN, U0420, Steering angle sensor malfunction');
-        expect(mockLogger.info).toHaveBeenCalledWith('2025-01-01 00:00:30, VEHICLE_ID:1004, INFO, P0171, System too lean (Bank 1)');
-        expect(mockLogger.info).toHaveBeenCalledWith('2025-01-01 00:00:35, VEHICLE_ID:1014, ERROR, U0420, Steering angle sensor malfunction');
+        expect(mockLogger.info).toHaveBeenCalledWith('Wed Jan 01 2025 00:00:25 GMT+0530 (India Standard Time), 1013, WARN, U0420, Steering angle sensor malfunction');
+        expect(mockLogger.info).toHaveBeenCalledWith('Wed Jan 01 2025 00:00:30 GMT+0530 (India Standard Time), 1004, INFO, P0171, System too lean (Bank 1)');
+        expect(mockLogger.info).toHaveBeenCalledWith('Wed Jan 01 2025 00:00:35 GMT+0530 (India Standard Time), 1014, ERROR, U0420, Steering angle sensor malfunction');
 
         expect(mockLogger.info).toHaveBeenCalledWith('3 records were parsed by the system');
     });
